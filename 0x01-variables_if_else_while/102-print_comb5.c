a8c877ee942f8c9d9b5a1a89ca6f99fa02255bdd
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-03-17 16:00:02 -0700

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+/**
+* main - Print combinations of two digit numbers
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	int j;
+	int n;
+	int m;
+	
+	for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			for (n = 0; n < 10; n++)
+			{
+				for (m = 0; m < 10; m++)
+				{
+					if (m > j || (m = j && n != i))
+					{
+						putchar('0' + i);
+						putchar('0' + j);
+						putchar(' ');
+						putchar('0' + n);
+						putchar('0' + m);
+						if (i != 9 || j != 8 || n != 9 || m != 9)
+						{
+							putchar(',');
+							putchar(' ');
+						}
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
