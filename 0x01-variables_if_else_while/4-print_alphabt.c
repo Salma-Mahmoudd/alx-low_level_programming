72a4ed2d31ebfd73232c2e8d2afd935518fe883c
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-03-17 13:01:43 -0700

    When I was having that alphabet soup, I never thought that it would pay off

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/**
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	
+	for (i = 97; i <= 122; i++)
+	{
+		if (i != 113 || i != 101)
+		{
+			putchar(i);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
