0436198e6f2ee4f9f422bc1d65730616d5f1c8dd
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-03-17 17:28:04 -0700

    Software is eating the World

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,34 +6,36 @@
 */
 int main(void)
 {
-	int i;
+	int i = 0;
 	int j;
 	int n;
 	int m;
 
-	for (i = 0; i < 10; i++)
+	for (j = 0; j < 10; j++)
 	{
-		for (j = 0; j < 10; j++)
+		for (n = 0; n < 10; n++)
 		{
-			for (n = 0; n < 10; n++)
+			for (m = 0; m < 10; m++)
 			{
-				for (m = 0; m < 10; m++)
+				if ((m < j && n > i) || (m > j && n >= i) || (m == j && n > i))
 				{
-					if ((m < j && n > i) || (m > j && n >= i) || (m == j && n > i))
+					putchar('0' + i);
+					putchar('0' + j);
+					putchar(' ');
+					putchar('0' + n);
+					putchar('0' + m);
+					if (i != 9 || j != 8 || n != 9 || m != 9)
 					{
-						putchar('0' + i);
-						putchar('0' + j);
+						putchar(',');
 						putchar(' ');
-						putchar('0' + n);
-						putchar('0' + m);
-						if (i != 9 || j != 8 || n != 9 || m != 9)
-						{
-							putchar(',');
-							putchar(' ');
-						}
 					}
 				}
 			}
+		}
+		if (i < 9 && j == 9)
+		{
+			j = 0;
+			i++;
 		}
 	}
 	putchar('\n');
