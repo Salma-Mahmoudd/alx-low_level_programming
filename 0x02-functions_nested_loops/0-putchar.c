e073b1d348fea20b7d1316df0150356771fc2ce9
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-03-21 13:34:35 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+* main - Print the word "Holberton"
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+	{
+		putchar(p[i]);
+	}
+	putchar('\n');
+	return (0);
+}
