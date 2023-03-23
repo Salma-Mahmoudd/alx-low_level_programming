c3fb14906453fdc6b2a56e0b5522ca73475de30e
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-03-23 08:09:29 -0700

    Triangles

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_triangle - prints a triangle(#)
+ *@size: the size of the triangle
+ * Return: Always 0.
+ */
+void print_
+ddtriangle(int size)
+{
+	int i, j;
+
+	if (size > 1)
+	{
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				if (j >= size - 1 - i)
+					_putchar(35);
+				else
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+	else
+		_putchar('\n');
+}
