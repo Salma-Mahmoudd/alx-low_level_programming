9715a29b2a810b12c49735fff925450d57ba2575
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-04-10 16:25:17 -0700

    Minimal Number of Coins for Change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,25 +19,27 @@ int main(int argc, char **argv)
 	else
 	{
 		cents = atoi(argv[1]);
-		if (cents < 0)
+		if (cents <= 0)
 			printf("%d\n", 0);
 		else
 		{
 			if (cents >= 25)
 			{
-				while (cents % 25)
+				coins += (cents / 25);
+				cents -= 25 * coins;
+				while (cents != 0)
 				{
-					if (!(cents % 10))
+					if (cents >= 10)
 					{
 						cents -= 10;
 						coins++;
 					}
-					else if (!(cents % 5))
+					else if (cents >= 5 && cents < 10)
 					{
 						cents -= 5;
 						coins++;
 					}
-					else if (!(cents % 2))
+					else if (cents >= 2 && cents < 5)
 					{
 						cents -= 2;
 						coins++;
@@ -48,18 +50,18 @@ int main(int argc, char **argv)
 						coins++;
 					}
 				}
-				coins += (cents / 25);
 			}
 			else if (cents >= 10 && cents < 25)
 			{
-				while (cents % 10)
+				coins += (cents / 10);
+				while (cents != 0)
 				{
-					if (!(cents % 5))
+					if (cents >= 5)
 					{
 						cents -= 5;
 						coins++;
 					}
-					else if (!(cents % 2))
+					else if (cents >= 2 && cents < 5)
 					{
 						cents -= 2;
 						coins++;
@@ -70,15 +72,17 @@ int main(int argc, char **argv)
 						coins++;
 					}
 				}
-				coins += (cents / 10);
 			}
 			else
 			{
 				while (cents != 0)
 				{
-					if (!(cents % 5))
+					if (cents >= 5 && cents < 10)
+					{
 						coins++;
-					else if (!(cents % 2))
+						cents -= 5;
+					}
+					else if (cents >= 2 && cents < 5)
 					{
 						cents -= 2;
 						coins++;
