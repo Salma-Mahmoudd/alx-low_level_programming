f54bdfceea0cbebc31ee09d6d5271ccb58c20b91
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-04-11 17:02:15 -0700

    If you even dream of beating me you`d better wake up and apologize

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -14,9 +14,20 @@ int **alloc_grid(int width, int height)
 
 	if (width <= 0 || height <= 0)
 		return (NULL);
-	grid = (int **)malloc(sizeof(int *) * width * height);
+	grid = (int **)malloc(sizeof(int *) * height);
 	if (grid == NULL)
 		return (NULL);
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(sizeof(int) * width);
+		if (grid[i] == NULL)
+		{
+			while (--i >= 0)
+				free(grid[i]);
+			free(grid);
+			return (NULL);
+		}
+	}
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
