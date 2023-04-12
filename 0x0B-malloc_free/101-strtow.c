703356e75f47b5c9b18d27f488c178580b066fa9
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-04-12 13:58:26 -0700

    I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
 /**
  * **strtow - splits a string into words.
@@ -10,19 +9,20 @@ char **strtow(char *str)
 {
 	int i = 0, j = 0, c = 0, x = 0, w = 0;
 	char **words;
+
 	if (str == NULL || str[0] == 0)
 		return (NULL);
 	while (str[i] != '\0')
 	{
 		if (str[i] != ' ')
 		{
-			if (str[i + 1] == ' ')
+			if (str[i + 1] == ' ' || str[i + 1] == '\0')
 				w++;
 		}
 		i++;
 	}
 	words = (char **)malloc(sizeof(char *) * w + 1);
-	if (words == NULL)
+	if (words == NULL || w < 1)
 		return (NULL);
 	i = 0;
 	while (str[i] != '\0')
@@ -37,19 +37,14 @@ char **strtow(char *str)
 				{
 					while (--c >= 0)
 						free(words[c]);
-					free(words);
-					return (NULL);
+					free(words), return (NULL);
 				}
-				for(x = 0; x < j; x++)
-				{
+				for (x = 0; x < j; x++)
 					words[c][x] = str[i + x + 1 - j];
-				}
-				words[c][x] = '\0';
-				c++, j = 0;
+				words[c][x] = '\0', c++, j = 0;
 			}
 		}
 		i++;
 	}
-	words[c] = '\0';
-	return (words);
+	words[c] = '\0', return (words);
 }
