404db6ebdc90b5b85ac83e002a73dba1e7d59b77
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-04-13 15:49:22 -0700

    _realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -15,7 +15,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
-	if (new_size == 0)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -31,7 +31,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	if (tmp == NULL)
 		return (NULL);
 	for (i = 0; i < old_size && i < new_size; i++)
-		tmp[i] = (char *)ptr[i];
+		tmp[i] = ((char *)ptr)[i];
 	free(ptr);
 	return (tmp);
 }
