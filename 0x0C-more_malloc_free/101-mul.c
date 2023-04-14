9ac202f5d71f43856b31c7ad562cec058e169ee8
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-04-13 18:24:09 -0700

    We must accept finite disappointment, but never lose infinite hope

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,9 +1,32 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <ctype.h>
 /**
- * *array_range - creates an array of integers.
- * @min: minimum int in array
- * @max: maximum int in array
- * Return: a pointer, or NULL if it fails.
+ * main - multiplies two positive numbers
+ * @argc: number of command line arguments
+ * @argv: one-dimensional array of strings
+ * Return: 0
  **/
+int main(int argc, char **argv)
+{
+	int i, j = 0;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	for (i = 0; i < argc; i++)
+	{
+		while (argv[i][j] != '\0')
+			j++;
+		if (!(isdigit(argv[i][j])))
+		{
+			printf("Error\n");
+			exit(98);
+		}
+		j = 0;
+	}
+	return (0);
+}
