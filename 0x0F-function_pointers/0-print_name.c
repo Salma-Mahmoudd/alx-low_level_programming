381f677168d962c54672e777e563902dc1497ff4
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-04-19 12:28:15 -0700

    C - Function pointers

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+/**
+ * print_name - function that prints a name.
+ * @name: name of the person
+ * @f: pointer to function that returns nothing
+ * Return: nothing
+ **/
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL || f != NULL)
+		f(name);
+}
