764b2e892a813f0c4e00334f1fe015ba8199a909
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-04-30 16:41:18 -0700

    Prime numbers

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,38 @@
+#include "main.h"
+/**
+ * _prime_number - find if given number is prime number
+ * @n: given number
+ * @test: test the mod operation
+ * Return: 1 or 0.
+ */
+int _prime_number(int n, int test)
+{
+	int count = 0;
+
+	if (test != 0)
+	{
+		if (n % test == 0)
+			count = 1 + _prime_number(n, test - 1);
+		else
+			count = _prime_number(n, test - 1);
+	}
+	return (count);
+}
+/**
+ * is_prime_number - find if given number is prime number
+ * @n: given number
+ * Return: 1 if the input integer is a prime number,
+ * otherwise return 0.
+ */
+int is_prime_number(int n)
+{
+	int count = 0, test = n;
+
+	if (n < 0)
+		n *= -1, test = n;
+	count = _prime_number(n, test);
+	if (count != 2 || n == 0)
+		return (0);
+	else
+		return (1);
+}
