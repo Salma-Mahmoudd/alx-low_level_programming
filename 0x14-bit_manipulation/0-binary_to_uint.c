98b41666d63af4c29a179d24ad9fd5ab7b8415dd
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-05-04 15:58:30 -0700

    Prototypes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,1 +1,18 @@
-#incli
+#include "main.h"
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int i = 1, dec = 0;
+	int len;
+
+	if (!b)
+		return (0);
+	len = strlen(b) - 1;
+	while (len != -1)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+		dec += (b[len--] - '0') * i;
+		i *= 2;
+	}
+	return (dec);
+}
