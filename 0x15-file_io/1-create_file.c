b0030808b05ba27d915af196e4a06dd02179ba5c
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-05-08 16:06:25 -0700

    creates a file and write in it

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -10,10 +10,10 @@ int create_file(const char *filename, char *text_content)
 {
 	int newFile, addedText = 0;
 
-	if (filename == NULL)
+	if (!filename)
 		return (-1);
 	newFile = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	if (!text_content)
+	if (text_content)
 		addedText = write(newFile, text_content, strlen(text_content));
 	if (newFile == -1 || addedText == -1)
 		return (-1);
