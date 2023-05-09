9c1e90e29049f650e7684296fc5e6a4b6513a643
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-05-09 15:58:24 -0700

    copies the content of a file to another file

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,7 +12,7 @@ int main(int ac, char **av)
 
 	if (ac != 3)
 	{
-		dprintf(2, "Usage: cp file_from file_to\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 	file = open(av[1], O_RDONLY);
@@ -21,15 +21,15 @@ int main(int ac, char **av)
 	{
 		tot_n = read(file, text, 1024);
 		if (file == -1 || tot_n == -1)
-			dprintf(2, "Error: Can't read from file %s\n", av[1]), exit(98);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 
 		actual_n = write(newFile, text, tot_n);
 		if (newFile == -1 || actual_n == -1)
-			dprintf(2, "Error: Can't write to %s\n", av[2]), exit(99);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
 	}
 	if (close(file) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", file), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file), exit(100);
 	if (close(newFile) == -1)
-		dprintf(2, "Error: Can't close fd %d\n", newFile), exit(100);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", newFile), exit(100);
 	return (0);
 }
