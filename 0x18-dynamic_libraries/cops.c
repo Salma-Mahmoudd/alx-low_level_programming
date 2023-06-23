1dbe0e9add67b8eec4a645cbda066b6afbbebfc4
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-06-23 07:27:26 -0700

    Call C functions from Python

---
+++ 0x18-dynamic_libraries/cops.c
@@ -0,0 +1,55 @@
+#include "main.h"
+/**
+ * add - function that sum two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result.
+ **/
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - function that substracte two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result.
+ **/
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - function that multiple two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result.
+ **/
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - function that divide two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result.
+ **/
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - function that module two numbers
+ * @a: first number
+ * @b: second number
+ * Return: the result.
+ **/
+int mod(int a, int b)
+{
+	return (a % b);
+}
