1874a36a4bfadc010012b983781eb0c4531ad33d
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-07-07 09:45:08 -0700

    Retrieves a value

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -1,9 +1,10 @@
 #include "hash_tables.h"
 /**
  * hash_table_get - retrieves a value associated with a key.
- * @ht: the hash table 
+ * @ht: the hash table
  * @key: the key you are looking for
- * Return: value associated with the element, or NULL if key couldn’t be found
+ * Return: value associated with the element,
+ * or NULL if key couldn’t be found
  **/
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
